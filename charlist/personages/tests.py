9f315c8ee0e6a3c0f934b2304ceec4390eaa4158
Anjel Ershova <ershova.a@skbkontur.ru>
2022-12-15 12:46:16 +0300

    Немного тестов

--- charlist/personages/tests.py
+++ charlist/personages/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from django.test.utils import setup_test_environment
+from django.test.utils import setup_test_environment, override_settings
 
-
+from myauth.models import CharlistUser
 from personages.models import Personage
 # Анжель, на досуге добавь тесты, как тут: https://developer.mozilla.org/ru/docs/Learn/Server-side/Django/Testing
 
@@ -12,14 +12,32 @@ class PersonageTest(TestCase):
         personages_count = Personage.objects.count()
         self.assertEqual(personages_count, 1)
 
+    @override_settings(DEBUG=True)
     def test_main_login_required(self):
         response = self.client.get('/all_personages')
-        self.assertEqual(302, response.status_code) # ошибка AssertionError: 302 != 301
+        # По умолчанию http клиент не проходит перенаправления. По этому проверяем так.
+        self.assertRedirects(response, '/all_personages/', status_code=301, target_status_code=302)
 
-        self.client.login(username='user_can_CRUD_personage', password='passwordCRUD')
-        response = self.client.get('/all_personages')  # force login
-        self.assertFalse(response.context['all_personages'].is_anonymous)
-        # self.assertIn('View all personages', response.content.decode()) # ошибка AssertionError: 'View all personages' not found in ''
-        print(response.content.decode())
+        # Создадим пользователя
+        u = CharlistUser(username="admin1", email="admin@localhost", is_active=True)
+        # Пароль надо задавать через метод. Т.к. в таблице хранятся хеши паролей.
+        u.set_password("1q2w3e")
+        u.save()
+
+        response = self.client.login(username='admin1', password="1q2w3e")  # force login
+        self.assertTrue(response)
+
+        # В отличие от response = self.client.get('/all_personages')
+        # тут нам нет смысла вручную обрабатывать редиректы. По этому просто ставим follow=True
+        # Т.е. проходить все редиректы автоматически.
+        response = self.client.get('/all_personages', follow=True)
         self.assertEqual(200, response.status_code)
 
+        # print(response.content.decode())
+
+    @override_settings(DEBUG=True)
+    def test_get_personage_detail(self):
+        self.client.login(username='user_can_CRUD_personage', password='passwordCRUD')
+        response = self.client.get('/personage/details/1/')
+        self.assertEqual(200, response.status_code)
+        self.assertIn('First', response.content.decode())
